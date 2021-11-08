99ea015bf57b8e5f7cda72cd6da964fbe69244c6
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2021-11-08 10:42:30 -0500

    27 - BehaviorTrees And Blackboards

--- Source/UnitedPlanets/UPGAIController.cpp
+++ Source/UnitedPlanets/UPGAIController.cpp
@@ -9,6 +9,11 @@ void AUPGAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+	}
+
 }
 
 void AUPGAIController::Tick(float DeltaSeconds)
@@ -16,16 +21,16 @@ void AUPGAIController::Tick(float DeltaSeconds)
 	Super::Tick(DeltaSeconds);
 
 	//declarations:
-		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	//	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-	if (LineOfSightTo(PlayerPawn))
-	{
-		SetFocus(PlayerPawn);
-		MoveToActor(PlayerPawn, AcceptanceRadius);
-	}
-	else
-	{
-		ClearFocus(EAIFocusPriority::Gameplay);
-		StopMovement();
-	}
+	//if (LineOfSightTo(PlayerPawn))
+	//{
+	//	SetFocus(PlayerPawn);
+	//	MoveToActor(PlayerPawn, AcceptanceRadius);
+	//}
+	//else
+	//{
+	//	ClearFocus(EAIFocusPriority::Gameplay);
+	//	StopMovement();
+	//}
 }	
